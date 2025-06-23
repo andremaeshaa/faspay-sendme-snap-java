0415ffa4aacbd30d24822cf1549551a43f2497f6
andremaesha <andremaesha@gmail.com>
2025-06-23 16:30:42 +0700

    Add `HistoryListClient` for transaction history functionality
    - Implemented `HistoryListClient` to handle the transaction history feature.
    - Integrated HTTP communication using `FaspaySnapHttpClient`.
    - Added null-checks, logging, and exception handling for robustness.

---
+++ src/main/java/id/co/faspay/snap/client/HistoryListClient.java
@@ -0,0 +1,42 @@
+package id.co.faspay.snap.client;
+
+import id.co.faspay.snap.config.FaspaySnapConfig;
+import id.co.faspay.snap.exception.FaspaySnapApiException;
+import id.co.faspay.snap.model.Constants;
+import id.co.faspay.snap.model.HistoryListRequest;
+import id.co.faspay.snap.model.HistoryListResponse;
+import id.co.faspay.snap.model.InquiryBalanceResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public class HistoryListClient {
+    private static final Logger logger = LoggerFactory.getLogger(HistoryListClient.class);
+    private final Constants constants;
+    private final FaspaySnapHttpClient httpClient;
+
+    public HistoryListClient(FaspaySnapConfig config) {
+        Objects.requireNonNull(config, "config must not be null");
+
+        this.httpClient = new FaspaySnapHttpClient(config);
+        this.constants = new Constants();
+    }
+
+    public HistoryListResponse historyList(HistoryListRequest request) throws FaspaySnapApiException {
+        Objects.requireNonNull(request, "request must not be null");
+
+        logger.info("Starting history list for account: {}", request.getAccountNo());
+
+        try {
+            HistoryListResponse response = httpClient.post(constants.getEndpointHistoryList(), constants.getUserAgent(), request, HistoryListResponse.class);
+
+            logger.info("history list completed with message: {}", response.getResponseMessage());
+
+            return response;
+        } catch (FaspaySnapApiException e) {
+            logger.error("Error inquiring about account: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
