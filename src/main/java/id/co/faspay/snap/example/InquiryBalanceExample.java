c0061045713cd00f8f34f4ce07a9cbfb9ef42a21
andremaesha <andremaesha@gmail.com>
2025-06-23 15:13:30 +0700

    Add `InquiryBalanceExample` for demonstrating balance inquiry usage
    - Introduced `InquiryBalanceExample` to showcase the usage of the balance inquiry API.
    - Includes example configuration, request creation, and response handling.
    - Demonstrates basic error handling and logging for clarity.

---
+++ src/main/java/id/co/faspay/snap/example/InquiryBalanceExample.java
@@ -0,0 +1,42 @@
+package id.co.faspay.snap.example;
+
+import id.co.faspay.snap.FaspaySnapClient;
+import id.co.faspay.snap.config.FaspaySnapConfig;
+import id.co.faspay.snap.exception.FaspaySnapApiException;
+import id.co.faspay.snap.model.InquiryBalanceRequest;
+import id.co.faspay.snap.model.InquiryBalanceResponse;
+import id.co.faspay.snap.model.StatusTransferRequest;
+import id.co.faspay.snap.model.StatusTransferResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class InquiryBalanceExample {
+    public static void main(String[] args) throws IOException {
+        URL resourceSsl = AccountInquiryExample.class.getResource("/faspay.crt");
+        URL privateKeyResource = AccountInquiryExample.class.getResource("/enc_stg.key");
+
+        assert privateKeyResource != null;
+        String privateKeyStr = Files.readString(new File(privateKeyResource.getFile()).toPath());
+
+        assert resourceSsl != null;
+        String sslString = Files.readString(new File(resourceSsl.getFile()).toPath());
+
+        String partnerId = "99999";
+
+        try {
+            FaspaySnapConfig config = new FaspaySnapConfig(partnerId, privateKeyStr, sslString);
+            config.setEnv("sandbox");
+            FaspaySnapClient client = new FaspaySnapClient(config);
+
+            InquiryBalanceRequest request = new InquiryBalanceRequest("9920017573");
+            InquiryBalanceResponse response = client.inquiryBalance().balance(request);
+
+            System.out.println("response code: " + response.getResponseCode());
+        } catch (FaspaySnapApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
