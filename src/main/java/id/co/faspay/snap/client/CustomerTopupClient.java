7b20a11d5839c18a521c12fd3e945d3f6aa011ae
andremaesha <andremaesha@gmail.com>
2025-06-24 14:26:39 +0700

    Add `CustomerTopupClient` to handle customer top-up operations
    - Introduced `CustomerTopupClient` for managing top-up transactions.
    - Integrated logging, null-checks, and exception handling for robustness.
    - Utilizes `FaspaySnapHttpClient` for API communication.

---
+++ src/main/java/id/co/faspay/snap/client/CustomerTopupClient.java
@@ -0,0 +1,40 @@
+package id.co.faspay.snap.client;
+
+import id.co.faspay.snap.config.FaspaySnapConfig;
+import id.co.faspay.snap.exception.FaspaySnapApiException;
+import id.co.faspay.snap.model.Constants;
+import id.co.faspay.snap.model.CustomerTopupRequest;
+import id.co.faspay.snap.model.CustomerTopupResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public class CustomerTopupClient {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerTopupClient.class);
+    private final Constants constants;
+    private final FaspaySnapHttpClient httpClient;
+
+    public CustomerTopupClient(FaspaySnapConfig config) {
+        Objects.requireNonNull(config, "config must not be null");
+        this.httpClient = new FaspaySnapHttpClient(config);
+        this.constants = new Constants();
+    }
+
+    public CustomerTopupResponse topup (CustomerTopupRequest request) throws FaspaySnapApiException {
+        logger.debug("Processing customer topup request: {}", request);
+
+        logger.info("customer topup about account {} at platform {}", request.getAdditionalInfo().getSourceAccount(), request.getAdditionalInfo().getPlatformCode());
+
+        try {
+            CustomerTopupResponse response = httpClient.post(constants.getEndpointCustomerTopup(), constants.getUserAgent(), request, CustomerTopupResponse.class);
+
+            logger.info("transfer status completed with response code: {}", response.getResponseCode());
+
+            return response;
+        } catch (FaspaySnapApiException e) {
+            logger.error("Error transfer status about account: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
