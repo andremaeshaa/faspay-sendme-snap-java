f4c4819e899bb35d52d1abe2519bd65236b7fda7
andremaesha <andremaesha@gmail.com>
2025-06-25 13:27:32 +0700

    Enhance `BillInquiryExample` with additional comments, request improvements, and response processing

--- src/main/java/id/co/faspay/snap/example/BillInquiryExample.java
+++ src/main/java/id/co/faspay/snap/example/BillInquiryExample.java
@@ -10,11 +10,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * Example demonstrating how to use the Bill Inquiry API.
+ * This example shows how to check details of a virtual account bill before making a payment.
+ */
 public class BillInquiryExample {
     public static void main(String[] args) throws IOException {
-        URL resourceSsl = AccountInquiryExample.class.getResource("/faspay.crt");
-        URL privateKeyResource = AccountInquiryExample.class.getResource("/enc_stg.key");
+        // Load SSL certificate and private key from resources
+        URL resourceSsl = BillInquiryExample.class.getResource("/faspay.crt");
+        URL privateKeyResource = BillInquiryExample.class.getResource("/enc_stg.key");
 
         assert privateKeyResource != null;
         String privateKeyStr = Files.readString(new File(privateKeyResource.getFile()).toPath());
@@ -22,26 +29,61 @@ public class BillInquiryExample {
         assert resourceSsl != null;
         String sslString = Files.readString(new File(resourceSsl.getFile()).toPath());
 
+        // Your partner ID provided by Faspay
         String partnerId = "99999";
 
         try {
+            // Create configuration
             FaspaySnapConfig config = new FaspaySnapConfig(partnerId, privateKeyStr, sslString);
-            config.setEnv("sandbox");
+            config.setEnv("sandbox"); // Use "production" for production environment
+
+            // Create client
             FaspaySnapClient client = new FaspaySnapClient(config);
 
+            // Generate a unique reference number
+            String referenceNo = "REF" + ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+
+            // Create additional info
             BillInquiryRequest.AdditionalInfo additionalInfo = new BillInquiryRequest.AdditionalInfo();
             additionalInfo.setBillerCode("013");
             additionalInfo.setSourceAccount("9920017573");
 
-            BillInquiryRequest request = new BillInquiryRequest("20250624151221271", "    7008", "08000047816", "700808000047816", additionalInfo);
+            // Create request
+            BillInquiryRequest request = new BillInquiryRequest(
+                referenceNo,        // Unique partner reference number
+                "7008",             // Partner service ID
+                "08000047816",      // Customer number
+                "700808000047816",  // Virtual account number
+                additionalInfo      // Additional info
+            );
 
+            // Perform bill inquiry
+            System.out.println("Performing bill inquiry for virtual account: " + request.getVirtualAccountNo());
             BillInquiryResponse response = client.billInquiry().inquiry(request);
 
+            // Process the response
             if (response.isSuccess()) {
-                System.out.println("responseCode: " + response.getResponseCode());
-                System.out.println("responseMessage: " + response.getResponseMessage());
+                System.out.println("Bill inquiry successful!");
+                System.out.println("Response code: " + response.getResponseCode());
+                System.out.println("Response message: " + response.getResponseMessage());
+
+                // Get virtual account data
+                BillInquiryResponse.VirtualAccountData vaData = response.getVirtualAccountData();
+                if (vaData != null) {
+                    System.out.println("Virtual Account Name: " + vaData.getVirtualAccountName());
+                    if (vaData.getTotalAmount() != null) {
+                        System.out.println("Total Amount: " + vaData.getTotalAmount().getValue() + " " + 
+                                                             vaData.getTotalAmount().getCurrency());
+                    }
+                    System.out.println("Partner Reference Number: " + vaData.getPartnerReferenceNo());
+                }
+            } else {
+                System.out.println("Bill inquiry failed!");
+                System.out.println("Response code: " + response.getResponseCode());
+                System.out.println("Response message: " + response.getResponseMessage());
             }
         } catch (FaspaySnapApiException e) {
+            System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
         }
     }
